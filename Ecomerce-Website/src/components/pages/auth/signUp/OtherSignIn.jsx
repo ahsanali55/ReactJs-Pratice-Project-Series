dec8eb7e7b652578d1aefea28b5be7e668ad008e
ahsanali555 <ahsanalijawad599@gmail.com>
2026-01-21 19:48:33 +0500

    Integrate authentication on google, facebook

--- Ecomerce-Website/src/components/pages/auth/signUp/OtherSignIn.jsx
+++ Ecomerce-Website/src/components/pages/auth/signUp/OtherSignIn.jsx
@@ -1,20 +1,51 @@
 import React from "react";
-import { FaApple, FaChevronLeft, FaEye, FaEyeSlash, FaFacebook } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../../../utils/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
+// => login with Facebook Code
+const facebookProvider = new FacebookAuthProvider();
+// facebookProvider.addScope("email"); // requesting email
+const handleFacebookLogin = async () => {
+  try {
+    await signInWithPopup(auth, facebookProvider);
+      navigate("/"); // navigate to home page
+  } catch (error) {
+    console.error(error);
+  }
+};
+// => login with Google Code
+const googleProvider = new GoogleAuthProvider();
+const navigate = useNavigate();
+const handleGoogleLogin = async () => {
+  try {
+    await signInWithPopup(auth, googleProvider);
+    // 🔥 No dispatch here
+    // onAuthStateChanged will handle Redux
+    navigate("/"); // navigate to home page
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const OtherSignIn = () => {
   return (
     <div className="text-center space-y-4 mt-3">
       <h3 className="text-gray-400">Or Sign in with</h3>
       <div className="flex items-center justify-center gap-2 ">
-        <div className="bg-[#f1f2f5] p-2 rounded-lg">
-          <FaFacebook className="text-blue-500 text-3xl" />
-        </div>
-        <div className="bg-[#f1f2f5] p-2 rounded-lg">
+        <div
+          onClick={handleGoogleLogin}
+          className="bg-[#f1f2f5] p-2 rounded-lg"
+        >
           <FcGoogle className="text-3xl" />
         </div>
-        <div className="bg-[#f1f2f5] p-2 rounded-lg ">
-          <FaApple className=" text-3xl" />
+        <div
+          onClick={handleFacebookLogin}
+          className="bg-[#f1f2f5] p-2 rounded-lg"
+        >
+          <FaFacebook className="text-blue-500 text-3xl" />
         </div>
       </div>
     </div>
